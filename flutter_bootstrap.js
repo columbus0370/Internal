bca15c33d2e7e7f9158831985e1289a8e1eb7e4d
columbus0370 <columbus0370@users.noreply.github.com>
2026-05-14 17:41:48 +0000

    deploy: 485e1a9371e43bccab3a098d7d8d2f9e2cab4070

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2418338166"
+    serviceWorkerVersion: "3321180195"
   }
 });
