9a13b5b960f05ea6e03a84c8d474b2371ef81e1c
takato <columbus0370@gmail.com>
2026-05-21 03:37:21 +0900

    Fix: Correct file encoding for Flutter withOpacity replacement
    - Previous replacement caused UTF-16 LE encoding issue
    - Re-applied replacement with explicit UTF-8 encoding
    - All 19 withValues(alpha:) → withOpacity() replacements now correct
    - Fixes compilation error: 'FormatException: Unexpected extension byte'

--- epl_match_simulator/lib/screens/match_simulation_screen.dart
+++ epl_match_simulator/lib/screens/match_simulation_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'dart:async';
 import '../models/match_prediction.dart';
 import '../models/simulation_event.dart';
--- epl_match_simulator/lib/widgets/goal_popup_widget.dart
+++ epl_match_simulator/lib/widgets/goal_popup_widget.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 
 class GoalPopupWidget extends StatefulWidget {
   final String scorerName;
